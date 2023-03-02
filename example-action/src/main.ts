9109e1ab1942b0ed8aea7bf8a12f7ff8711e8b8d
Hidetake Iwata <int128@gmail.com>
2023-03-02 12:04:06 +0900

    Post comment

--- example-action/src/main.ts
+++ example-action/src/main.ts
@@ -5,7 +5,17 @@ import * as github from '@actions/github'
 const main = async () => {
   core.info(`Hello world from ref ${github.context.ref}`)
 
-  await exec.exec('git', ['version'])
+  const gitVersion = await exec.getExecOutput('git', ['version'])
+
+  core.info(`Posting a comment`)
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN ?? '')
+  const { data: comment } = await octokit.rest.issues.createComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.issue.number,
+    body: gitVersion.stdout,
+  })
+  core.info(`Posted as ${comment.html_url}`)
 }
 
 main().catch((e) => core.setFailed(e instanceof Error ? e : String(e)))
